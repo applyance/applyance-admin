c360d0e73e0ec73a8ef721176df1d23f66e35c70
Stephen Watkins <stjowa@gmail.com>
2014-09-14 16:06:15 -0500

    Adding application URL to entities.

--- assets/scripts/controllers/entities.js
+++ assets/scripts/controllers/entities.js
@@ -1,13 +1,21 @@
 'use strict';
 
 module.exports = angular.module('Admin')
-  .controller('EntitiesCtrl', ['$scope', 'Store', 'ApplyanceAPI',
-    function ($scope, Store, ApplyanceAPI) {
+  .controller('EntitiesCtrl', ['$scope', 'Store', 'ApplyanceAPI', 'client_url',
+    function ($scope, Store, ApplyanceAPI, client_url) {
 
       $scope.entities = [];
       ApplyanceAPI.getAllEntities().then(function(entities) {
         $scope.entities = entities.plain();
       });
+
+      $scope.applicationUrl = function(entity) {
+        var url = "/";
+        if (entity.parent) {
+          url += entity.parent.slug + "/";
+        }
+        return client_url + url + entity.slug;
+      };
 
     }
   ]);
